ca063266d6a11421d7c68197fa1713b26b77b97c
Graciela Torres <gracielatorres@gmail.com>
2019-02-11 19:27:15 -0300

    First commit.

---
+++ hellocucumber/features/step_definitions/stepdefs.js
@@ -0,0 +1,18 @@
+const helper = require('../helpers/fridayHelper');
+const assert = require('assert');
+const { Given, When, Then } = require('cucumber');
+
+
+
+Given('today is {string}', function (givenDay) {
+  this.today = givenDay;
+});
+
+When('I ask whether it\'s Friday yet', function () {
+  this.actualAnswer = helper.isItFriday(this.today);
+});
+
+Then('I should be told {string}', function (expectedAnswer) {
+  assert.equal(this.actualAnswer, expectedAnswer);
+});
+
